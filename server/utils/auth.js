57da13a5ab28da4fb15ccb5505f6cbd3bf45feaf
CoCo Chanel <veta583518@gmail.com>
2021-06-09 21:43:09 -0500

    signup not working

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,7 +8,7 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
@@ -30,7 +30,7 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    // next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
